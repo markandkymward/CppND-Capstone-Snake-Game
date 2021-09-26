05e83379d8fa17fb2e41d8295d3b4a2be4c224df
Mark D Ward <e331095@SACSTRL1105CF7-LNX.us.lmco.com>
2021-09-25 23:03:20 -0400

    multiple food

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/markward/git/CppND-Capstone-Snake-Game
+# Install script for directory: /home/e331095/git/workspace/CppND-Capstone-Snake-Game
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/markward/git/CppND-Capstone-Snake-Game/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/e331095/git/workspace/CppND-Capstone-Snake-Game/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
